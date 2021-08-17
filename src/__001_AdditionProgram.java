19b5dedf0719b2baa4ddef59f75e66116f31d984
iunura <uzenirem@gmail.com>
2021-08-17 21:12:32 +0300

    commit

---
+++ src/__001_AdditionProgram.java
@@ -0,0 +1,13 @@
+public class __001_AdditionProgram {
+
+    public static void main(String[] args) {
+        int a, b,c;
+
+        a = 18;
+        b = 20;
+
+        c = a+b;
+        System.out.println ("Toplam="+ c);
+	// write your code here
+    }
+}
---
+++ src/__002_AdditionProgramKeyboard.java
@@ -0,0 +1,16 @@
+public class __002_AdditionProgramKeyboard {
+    public static void main(String[] args) {
+        java.util.Scanner kb = new java.util.Scanner(System.in);
+        int a, b ,c;
+
+        System.out.print("a'nın değerini giriniz :");
+        a = kb.nextInt();
+
+        System.out.print("b'nin değerni giriniz : ");
+        b = kb.nextInt();
+
+        c = a + b;
+        System.out.print("Toplam = " + c);
+
+    }
+}
